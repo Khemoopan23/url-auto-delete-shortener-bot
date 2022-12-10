0092e209423af3730dfc5c20fa923fd23807febc
lx pandey <114104576+Khemoopan23@users.noreply.github.com>
2022-12-10 18:26:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,15 @@
 import re
 from os import environ
 
-
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+    
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23560088'))
